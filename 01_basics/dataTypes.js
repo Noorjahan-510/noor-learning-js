1c658fb64bede74054d5d0eb0ec6ef7b82bfb533
Noorjahan Khatoon <38237183+Noorjahan-510@users.noreply.github.com>
2024-07-09 10:19:57 +0000

    data types done

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,27 @@
+"use restrict" //treat all JS code as newer version
+
+//alert(3+3); // error, we are using nodejs,not browser
+
+
+console.log(3+3);
+
+console.log("Noor");
+
+let name="Noor";
+let age=24;
+let isLoggedIn= true;
+let state;
+
+//number  : 2 to power 53
+//big int
+//string : ""
+//boolean :true/false
+//null :standalone value
+//undefined :
+//symbol :unique
+
+//object
+
+console.log(typeof "Noor");
+console.log(typeof null);   //object
+console.log(typeof undefined)  // undefined

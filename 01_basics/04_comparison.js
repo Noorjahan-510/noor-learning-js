2dd730a09df506c85e10c17f0a2135556e01f77d
Noorjahan Khatoon <38237183+Noorjahan-510@users.noreply.github.com>
2024-07-09 11:57:39 +0000

    comparison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+ console.log("2"==2);  //true
+// console.log("02">1);  //true
+
+ //compare with same data types
+
+//  console.log(null>0);
+//  console.log(null==0);
+//  console.log(null>=0);
+
+//  console.log(undefined>=0);
+//  console.log(undefined==0);
+//  console.log(undefined<0);
+
+console.log(" === : strict check ");
+
+console.log("2"===2);  // === : it will compare along with data types

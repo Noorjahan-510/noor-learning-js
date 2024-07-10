91ae5b0ddc3ab78af029a8b636db7edc343af51d
Noorjahan Khatoon <38237183+Noorjahan-510@users.noreply.github.com>
2024-07-10 19:49:49 +0000

    objects done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,34 @@
+//singleton
+
+//*if litrals ki trh declare karte h to singleton nhi bnta hai
+
+//object litrals
+const mysymbol= Symbol("key1");
+const JsUser={
+    name:"Noor",
+    age:"21",
+    location:"gurgaon",
+    isLoggedyIn:false,
+    gmail:"noor@google.com",
+    loginDays:["Mon","wed"],
+    "last name":"khatoon",
+    [mysymbol]:"key1"       //syntax imp*
+}
+
+console.log(JsUser.gmail);
+console.log(JsUser["gmail"]);
+console.log(JsUser["last name"]);
+console.log(typeof JsUser[mysymbol]);  //*syntax
+console.log(typeof mysymbol);
+
+
+JsUser.age=24;
+//Object.freeze(JsUser);          //freez the object, after freezing changes wont be done
+JsUser.age=26;
+console.log(JsUser);
+
+JsUser.greetings=function(){
+    console.log(`Namaste ${this.name}!`)
+}
+
+console.log(JsUser.greetings());

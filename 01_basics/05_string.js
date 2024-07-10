c2dc3c73f96331b85575d05a4122bf19777612b4
Noorjahan Khatoon <38237183+Noorjahan-510@users.noreply.github.com>
2024-07-10 12:16:29 +0000

    String learned

---
+++ 01_basics/05_string.js
@@ -0,0 +1,33 @@
+const name="Noorjahan";
+const id=3456;
+
+console.log(`my name is ${name} and my emp id is ${id}`);
+
+const newStr="noorjahan-khatoon";
+
+console.log(newStr[1]);
+console.log(newStr.__proto__);
+
+
+console.log(newStr.length)
+console.log(newStr.toUpperCase());
+console.log(newStr.indexOf('r'));
+console.log(newStr.charAt(8));
+
+//const newString = newStr.substring(0,9);
+
+console.log(newStr.substring(0,9));
+
+
+console.log(newStr.slice(-17,4));
+
+const newname="     sachin     "
+console.log(newname);
+console.log(newname.trim());
+
+console.log(newStr.replace('noor','shah'));
+
+console.log(newStr.split('-'));
+
+console.log(newStr.slice(0,4));
+

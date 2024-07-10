b5addcf18f5ecd0ade391aef68b13fad8a314b0f
Noorjahan Khatoon <38237183+Noorjahan-510@users.noreply.github.com>
2024-07-10 10:34:47 +0000

    changes in this file

--- 01_basics/first.js
+++ 01_basics/first.js
@@ -1,1 +1,20 @@
 console.log("Hello Noor, happy learning")
+
+//const a;  //const declaration must be inialized.
+//console.log(a);
+
+
+
+// let a=10;
+// var b=20;
+
+// {
+//     var b=30;
+//     console.log(b);
+// }
+// console.log(b);
+
+
+
+console.log(name);
+let name="sachin";

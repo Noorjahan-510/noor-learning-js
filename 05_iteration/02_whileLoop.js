dd9e91ec858ab9ecc438ba8d8aa121b0a4c0792e
Noorjahan Khatoon <38237183+Noorjahan-510@users.noreply.github.com>
2024-07-15 09:31:16 +0000

    while and do..while loop

---
+++ 05_iteration/02_whileLoop.js
@@ -0,0 +1,21 @@
+let index=0;
+while(index<=10){
+    console.log(`value is ${index}`);
+    index=index+2
+}
+
+
+let arr=["virat","rohit","yuvraj"]
+let marr=0;
+while(marr<arr.length){
+    console.log(`value of array is: ${arr}`);
+    marr=marr+1;
+}
+
+
+let score=1;
+
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=10);
